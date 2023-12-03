e7e3818df8893e9c8f17dbd60dbfb72a4c8f5e57
mateagluhak <mg51820@fer.hr>
2023-12-03 17:09:59 +0100

    filled colors table

--- KidLingo/src/main/resources/db.migration/V2__fill.sql
+++ KidLingo/src/main/resources/db.migration/V2__fill.sql
@@ -1,3 +1,12 @@
 INSERT INTO dictionary(english, french, italian, german, category) VALUES
 ('red', 'rouge', 'rosso', 'rot', 'colors'),
-('blue', 'bleu', 'blu',	'blau', 'colors');
+('blue', 'bleu', 'blu', 'blau', 'colors'),
+('green', 'vert', 'verde', 'grün', 'colors'),
+('yellow', 'jaune', 'giallo', 'gelb', 'colors'),
+('orange', 'orange', 'arancione', 'orange', 'colors'),
+('pink', 'rose', 'rosa', 'rosa', 'colors'),
+('purple', 'violet', 'viola', 'lila', 'colors'),
+('brown', 'marron', 'marrone', 'braun', 'colors'),
+('white', 'blanc', 'bianco', 'weiß', 'colors'),
+('gray', 'gris', 'grigio', 'grau', 'colors'),
+('black', 'noir', 'nero', 'schwarz', 'colors');
