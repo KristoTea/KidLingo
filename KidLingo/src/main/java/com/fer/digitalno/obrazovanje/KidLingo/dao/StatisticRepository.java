6b4743423bdf5852b5ebca2fc01e66b33935213e
BernardKazazic <bernard.kazazic@gmail.com>
2024-01-17 00:41:55 +0100

    limited statistics to last 20 game results

--- KidLingo/src/main/java/com/fer/digitalno/obrazovanje/KidLingo/dao/StatisticRepository.java
+++ KidLingo/src/main/java/com/fer/digitalno/obrazovanje/KidLingo/dao/StatisticRepository.java
@@ -10,10 +10,10 @@ import java.util.List;
 @Repository
 public interface StatisticRepository extends JpaRepository<Statistic, Long> {
 
-    @Query(value = "SELECT number_of_correct_answers FROM statistic WHERE language = ?1 AND level = ?2 AND category = ?3", nativeQuery = true)
+    @Query(value = "SELECT number_of_correct_answers FROM statistic WHERE language = ?1 AND level = ?2 AND category = ?3 ORDER BY play_timestamp DESC LIMIT 20", nativeQuery = true)
     List<Integer> findAllNumberOfCorrectAnswersByLanguageAndLevelAndCategory(String language, String level, String category);
 
-    @Query(value = "SELECT solving_speed FROM statistic WHERE language = ?1 AND level = ?2 AND category = ?3", nativeQuery = true)
+    @Query(value = "SELECT solving_speed FROM statistic WHERE language = ?1 AND level = ?2 AND category = ?3 ORDER BY play_timestamp DESC LIMIT 20", nativeQuery = true)
     List<Double> findAllSolvingSpeedByLanguageAndLevelAndCategory(String language, String level, String category);
 
     List<Statistic> findAllByLanguageOrderByPlayTimestamp(String language);
